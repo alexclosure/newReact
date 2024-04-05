04ead3088ece584160f1a5fa54f41ee70d2ee45c
Alexandr Golov <alxglv@gmail.com>
2024-04-05 22:08:15 +0300

    Fix 'integer nubmers only' for a first components task

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react"
 import styles from "./modules/App.module.css"
 import AppComponent from "./components/AppComponent"
-import { useState } from "react"
 
 const App = () => {
-	const [state, setState] = useState({ value: "", list: [], error: "" })
-
-	const onInputButtonClick = () => {
-		const value = +prompt("Enter value").trim().toFixed(0)
-		console.log("value", value)
-	}
-
 	return (
 		<div className={styles.app}>
 			<AppComponent />
--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -7,8 +7,8 @@ const AppComponent = () => {
 	const [error, setError] = useState("")
 
 	const onInputButtonClick = () => {
-		const promptValue = +prompt("Enter value")
-		if (promptValue.toFixed().length < 3) {
+		const promptValue = prompt("Enter value")
+		if (promptValue.length < 3) {
 			setError("Введенное значение должно содержать минимум 3 символа")
 		} else {
 			setValue(promptValue)
@@ -56,7 +56,7 @@ const AppComponent = () => {
 			<p className="no-margin-text">
 				Текущее значение <code>value</code>: "<output className="current-value">{value}</output>"
 			</p>{" "}
-			{error && <div className="error">{error}</div>}
+			{error && <div className={styles.error + " " + styles.textDanger}>{error}</div>}{" "}
 			<div className="buttons-container">
 				<button className="button" onClick={onInputButtonClick}>
 					Ввести новое
@@ -68,7 +68,6 @@ const AppComponent = () => {
 			<div className="list-container">
 				<h2 className="list-heading">Список:</h2>
 				{list.length > 0 ? renderList() : <p className="no-margin-text">Нет добавленных элементов</p>}
-				<ul className="list">{list.length > 0 ? null : <li className="list-item">Первый элемент</li>}</ul>
 			</div>
 		</div>
 	)
