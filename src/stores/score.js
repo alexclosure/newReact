54007852a9cd9fb9fc6188d50770d1fb960e3b46
Alexandr Golov <alxglv@gmail.com>
2024-10-21 21:41:55 +0300

    First Init with docker

--- src/stores/score.js
+++ src/stores/score.js
@@ -1,7 +1,47 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
+const baseLevelScore = 25;
 
-export const useScoreStore = defineStore('score', {
+const levels = new Array(15)
+  .fill(0)
+  .map((_, i) => baseLevelScore * Math.pow(2, i));
+
+function computeLevelByScore(score) {
+  for (let [i, value] of levelScores.entries()) {
+    if (score <= value) {
+      return {
+        level: i,
+        value: levels[i],
+      };
+    }
+  }
+}
+console.log(levels);
+const levelScores = levels.map((_, level) => {
+  let sum = 0;
+  for (let [index, value] of levels.entries()) {
+    if (index >= level) {
+      return sum + value;
+    }
+    sum += value;
+  }
+  return sum;
+});
+export const useScoreStore = defineStore("score", {
   state: () => ({
     score: 0,
   }),
-})
+  getters: {
+    level: (state) => computeLevelByScore(state.score),
+    currentScore(state) {
+      if (this.level.level === 0) {
+        return state.score;
+      }
+      return state.score - levelScores[this.level.level - 1];
+    },
+  },
+  actions: {
+    add(score = 1) {
+      this.score += score;
+    },
+  },
+});
